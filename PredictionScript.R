b94eabe8162e52ff8176e8700e22e2402f48083f
Jason G <jasondouglasgibbs@gmail.com>
2021-07-02 16:05:31 -0400

    Updated files.

--- PredictionScript.R
+++ PredictionScript.R
@@ -49,33 +49,32 @@ blogs<-readLines("./data/final/en_US/en_US.blogs.txt",warn=FALSE,encoding="UTF-8
 news<-readLines("./data/final/en_US/en_US.news.txt",warn=FALSE,encoding="UTF-8")
 
 ###########################################Sampling the data###################################
-##Take 20,000 samples from each category.
+##Take samples from each category.
 set.seed(37)
-DataSample<-c(sample(twitter, 30000), sample (blogs, 30000), sample(news,30000))
-
-###########################################Precleaning###################################
-##Create/clean corpus##
-##Removes ASCII, etc.##
+DataSample<-c(sample(twitter, 35000), sample (blogs, 35000), sample(news,35000))
 DataSample<-iconv(DataSample,"latin1","ASCII", sub="")
-
-##Creates corpus using quanteda package.
-#corpus<-quanteda::corpus(DataSample)
-##Turns all characters to lowercase.
+###########################################Precleaning###################################
+##Reads profanity file##
 profanities <- readLines('profanity.txt')
-StopWords<-stopwords()
-
+##Creates intial corpus and transforms to data frame for initial cleaning.##
 corpus <- VCorpus(VectorSource(DataSample))
 dataframe<-data.frame(text=unlist(sapply(corpus, "content")), stringsAsFactors=F)
-dataframe$text<-tolower(dataframe$text)
+dataframe$text<-tolower(dataframe$text) ##Changes corpus to lower case.##
 
+##Filters profanities##
 for (i in 1:length(profanities)){
   dataframe$text<-gsub(profanities[i],"", dataframe$text)
 }
 
-
+##Filters out special characters and common numbers.##
 dataframe$text <- gsub("[][?!#$%()*,.:;<=>@^_|~.{}]", "", dataframe$text)
 dataframe$text <- gsub("\\\\", "", dataframe$text)
 dataframe$text <- gsub('\"', "", dataframe$text, fixed=T)
+dataframe$text <- gsub('one', "", dataframe$text)
+dataframe$text <- gsub('two', "", dataframe$text)
+dataframe$text <- gsub('three', "", dataframe$text)
+dataframe$text <- gsub('four', "", dataframe$text)
+dataframe$text <- gsub('five', "", dataframe$text)
 corpus<-VCorpus(VectorSource(dataframe$text))
 
 
@@ -85,42 +84,54 @@ toSpace <- content_transformer(function(x, pattern) {return (gsub(pattern," ",x)
 corpus <- tm_map(corpus,toSpace,"[^[:graph:]]")
 #Transforming all data to lower case
 corpus <- tm_map(corpus,content_transformer(tolower))
-#Deleting all English stopwords and any stray letters left my the non-ASCII removal
+#Deleting all English stopwords and any stray letters
 corpus <- tm_map(corpus,removeWords,c(stopwords("english"),letters))
 #Removing Punctuation
 corpus <- tm_map(corpus,removePunctuation)
 #Removing Numbers
 corpus <- tm_map(corpus,removeNumbers)
-
-#Removing all stray letters left by the last two calls
+#Removing all stray letters
 corpus <- tm_map(corpus,removeWords,letters)
 #Striping all extra whitespace
 corpus <- tm_map(corpus,stripWhitespace)
 
+##Converts corpus to a quanteda corpus for use in the SBO package.##
 corpus<-quanteda::corpus(corpus)
 
 
-write.table(corpus, file="PredictionCorpus.txt")
-
+##Creates prediction model for the corpus using the SBO package.##
 p1<-sbo_predictor(corpus, dict=target~0.75, .preprocess = sbo::preprocess, EOS = ".?!:;", N=3)
-predict(p1, "president")
-
+predict(p1, "mail")
 summary(p1)
 
 
-##Code for use on Shiny App##
+##Saves corpus as an .rda file for use on Shiny App.##
 corpus_trained<-sbo_predtable(object = corpus, N = 3, dict = target ~ 0.75, .preprocess = sbo::preprocess, 
                               EOS = ".?!:;")
 save(corpus_trained, file="corpus_trained.rda")
 
+##Code for use on Shiny App##
 load("corpus_trained.rda")
 corpus_trained2<-sbo_predictor(corpus_trained)
 predict(corpus_trained2, "dancing")
-predictvector<-predict(corpus_trained2, "new york")
-if (predictvector[1]=="<EOS>"){
-  predictvector<-predictvector[2]
+predictvector<-predict(corpus_trained2, "new york city")
+if (predictvector[1]=="<EOS>"&&predictvector[2]!="said"&&predictvector[3]!="will"){
+  predictvector[1]<-predictvector[2]
   
 }
-predictvector[1]
+if (predictvector[1]=="<EOS>"&&predictvector[2]!="said"&&predictvector[3]!="will"){
+  predictvector[1]<-predictvector[3]
+  
+}
 
-stopwords()
+if (predictvector[1]=="<EOS>"&&predictvector[2]!="said"&&predictvector[3]!="will"){
+  predictvector[1]<-"No prediction available."
+  
+}
+
+if (predictvector[1]=="<EOS>"&&predictvector[2]=="said"&&predictvector[3]=="will"){
+  predictvector[1]<-"said"
+  
+}
+
+predictvector[1]
